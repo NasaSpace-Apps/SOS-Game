8735ce0c6d945529a123f2c60e85a09c725dabda
sarah012-210 <148900173+sarah012-210@users.noreply.github.com>
2024-10-01 08:45:52 +0200

    Basics Game

---
+++ Game/Assets/C# scripts/Age.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Age : MonoBehaviour
+{
+  
+    public Slider slider;
+    public TextMeshProUGUI valueText;
+    public Button playButton;
+
+    void Start()
+    {
+        slider.minValue = 3;
+        slider.maxValue = 90;
+
+        slider.onValueChanged.AddListener(UpdateValueText);
+
+        playButton.onClick.AddListener(OnPlayButtonClick);
+
+        UpdateValueText(slider.value);
+    }
+    void UpdateValueText(float value)
+    {
+        valueText.text = value.ToString("0");
+    }
+
+    void OnPlayButtonClick()
+    {
+        float sliderValue = slider.value;
+
+        if (sliderValue <= 10)
+        {
+            SceneManager.LoadScene("Childs");
+        }
+        else
+        {
+            SceneManager.LoadScene("Adults");
+        }
+    }
+}
